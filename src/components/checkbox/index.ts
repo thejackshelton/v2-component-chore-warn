e086dca6866259ab7182166b87225ec3bcda6693
thejackshelton <me@jackshelton.com>
2025-10-21 18:21:32 -0500

    simpler

--- src/components/checkbox/index.ts
+++ src/components/checkbox/index.ts
@@ -1,13 +1,9 @@
 import { CheckboxRoot } from "./checkbox-root";
-import { CheckboxIndicator } from "./checkbox-indicator";
 import { CheckboxTrigger } from "./checkbox-trigger";
-import { CheckboxDescription } from "./checkbox-description";
 import { CheckboxError } from "./checkbox-error";
 
 export const Checkbox = {
   Root: CheckboxRoot,
-  Indicator: CheckboxIndicator,
   Trigger: CheckboxTrigger,
-  Description: CheckboxDescription,
   Error: CheckboxError,
 };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,31 +1,14 @@
 import type { DocumentHead } from "@qwik.dev/router";
 
 import { Checkbox } from "~/components/checkbox";
-import { component$, useSignal } from "@qwik.dev/core";
+import { component$ } from "@qwik.dev/core";
 
 export default component$(() => {
-  const isError = useSignal(true);
-
-  const isRendered = useSignal(true);
-
   return (
-    <>
-      <Checkbox.Root>
-        <Checkbox.Trigger class="size-10 bg-yellow-500 ui-checked:bg-red-500">
-          <Checkbox.Indicator class="checkbox-indicator">
-            Checked
-          </Checkbox.Indicator>
-        </Checkbox.Trigger>
-        <Checkbox.Description>Description</Checkbox.Description>
-        {isError.value && <Checkbox.Error>Error</Checkbox.Error>}
-      </Checkbox.Root>
-      <button onClick$={() => (isError.value = !isError.value)}>
-        Toggle Error
-      </button>
-      <button onClick$={() => (isRendered.value = !isRendered.value)}>
-        Checkbox Rendered
-      </button>
-    </>
+    <Checkbox.Root>
+      <Checkbox.Trigger class="size-10 bg-yellow-500 ui-checked:bg-red-500" />
+      <Checkbox.Error>Error</Checkbox.Error>
+    </Checkbox.Root>
   );
 });
 
